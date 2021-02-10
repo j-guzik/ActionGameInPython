1eb7ba054d1c8297125ebe317e4c47582adf57b1
j-guzik <justyna.b.guzik@gmail.com>
2021-02-10 23:45:13 +0100

    Add class Player

--- game_window.py
+++ game_window.py
@@ -9,6 +9,27 @@ pygame.display.set_caption("Survive and collect")
 clock = pygame.time.Clock()
 BACKGROUND = pygame.image.load(os.path.join('png', r"\image\background.png")).convert()
 
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, file_image):
+        super().__init__()
+        self.image = file_image
+        self.rect = self.image.get_rect()
+        self.items = {}
+        self.movement_x = 0
+        self.movement_y = 0
+        self._count = 0
+        self.lifes = 3
+        self.level = None
+        self.direction_of_movement = 'right'
+        self.weapon = 0
+        self.fairy1 = 0
+
+
+def draw(self, surface):
+    surface.blit(self.image, self.rect)
+
 window_open = True
 while window_open:
     screen.fill(gm.LIGHTBLUE)
