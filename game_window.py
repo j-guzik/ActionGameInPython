926a9652b25330f5867851f19f53842381c162c9
j-guzik <justyna.b.guzik@gmail.com>
2021-02-18 19:13:18 +0100

    player move

--- game_window.py
+++ game_window.py
@@ -27,10 +27,82 @@ class Player(pygame.sprite.Sprite):
         self.fairy1 = 0
 
 
-def draw(self, surface):
-    surface.blit(self.image, self.rect)
+    def turn_right(self):
+        self.direction_of_movement = 'right'
+        self.movement_x = 9
 
+    def turn_left(self):
+        self.direction_of_movement = 'left'
+        self.movement_x = -9
+
+    def stop(self):
+        self.movement_x = 0
+
+    def get_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                self.turn_right()
+            if event.key == pygame.K_a:
+                self.turn_left()
+            if event.key == pygame.K_w:
+                self.jump()
+            if event.key == pygame.K_f:
+                self.shoot()
+
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d and self.movement_x > 0:
+                self.stop()
+            if event.key == pygame.K_a and self.movement_x < 0:
+                self.stop()
+
+    def update(self):
+        self._gravitation()
+
+        self.rect.x += self.movement_x
+
+        if self.movement_x > 0:
+            self._move(gm.IMAGES_R)
+        if self.movement_x < 0:
+            self._move(gm.IMAGES_L)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    def _move(self, image_list):
+        if self._count < 1:
+            self.image = image_list[0]
+        elif self._count < 2:
+            self.image = image_list[1]
+        elif self._count < 3:
+            self.image = image_list[2]
+        elif self._count < 4:
+            self.image = image_list[3]
+        elif self._count < 5:
+            self.image = image_list[4]
+        elif self._count < 6:
+            self.image = image_list[5]
+        elif self._count < 7:
+            self.image = image_list[6]
+        elif self._count < 8:
+            self.image = image_list[7]
+        elif self._count < 9:
+            self.image = image_list[8]
+        elif self._count < 10:
+            self.image = image_list[9]
+        if self._count >= 10:
+            self._count = 0
+        else:
+            self._count += 1
+
+
+# konkretyzacja obiektow
+player = Player(gm.STAND_R)
+player.rect.center = screen.get_rect().center
+
+# glowna petla
 window_open = True
+active_game = False
 while window_open:
     screen.fill(gm.LIGHTBLUE)
     # pętla zdarzeń
